2c5759b5354c6fa749d6eec15a45e9f4a7260e46
foleygoalie <mfoley419@gmail.com>
2022-03-26 00:28:55 -0400

    mvp design done

---
+++ index.js
@@ -0,0 +1,26 @@
+let getDragonBtn = document.getElementById("get-dragon-button");
+let hideDragonBtn = document.getElementById("hide-dragon-button");
+let dragonNest = document.getElementById("dragon-nest");
+
+const fetchData = () => {
+    fetch("https://thesimpsonsquoteapi.glitch.me/quotes?count=50")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        data.forEach((element) => {
+                renderData(element)
+        });
+    });
+};
+
+const renderData = (obj) => {
+  let img =  document.createElement("img")
+  img.src = obj.image
+  img.className = "body-box-1"
+  dragonNest.append(img)
+
+
+}
+
+fetchData()
